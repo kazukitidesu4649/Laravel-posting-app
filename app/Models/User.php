c7f8641994798f4399773117a919f749a8b22316
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-11-30 09:45:51 +0900

    index.blade 削除機能を追加 onsubmit属性を設定し、確認ダイアログを表示

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
